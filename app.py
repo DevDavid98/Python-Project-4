1a9a2b0e43a66e21e66a1f09a0118ffac77eab2c
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2020-11-01 05:13:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,33 @@ import sys
 import re
 
 
-
-
-
 #create database name and db
 db = SqliteDatabase('inventory.db')
+
+
 class Product(Model):
-    product_names = CharField()
-    product_quantities = IntegerField()
-    product_prices = IntegerField()
-    product_dates = DateTimeField()
-    product_id = primary_key()
+    #product_names = CharField(unique = True)
+    product_quantity = IntegerField(unique = True)
     class Meta():
         database = db
-    
-def all_product_names():
-    pass
+
+
+#def view_all_inventory():
+#    all_products = inventory[3]
+#    try:
+#        product_records = Product.create(product_names = all_products)
+#        product_records.save()
+#    except IntegrityError:
+#        records = Product.get(product_names = all_products)
+        
+                
+def all_product_stock():
+    for the_stock in inventory[0:1]:
+        for item in the_stock.values():
+            for all_quantities in item:
+                product_stock = Product.create(product_quantity = all_quantities)
+                product_stock.save()
+
 def all_product_prices():
     pass
 def all_product_dates():
@@ -30,9 +41,11 @@ def all_product_ids():
 def delete_products():
     pass
 def add_products():
-    pass
-def edit_products():
-    pass
+    print('Hello, what item would you like to add?')
+    #add name
+    #how many
+    #create date added
+
 
 
 #creates main menu
@@ -62,10 +75,12 @@ def main_menu():
         else:
             print('INVALID OPTION')
             print('Please enter valid a option')
-
+            
+            
 
 #gets csv information
 #stores csv info as divt within list
+
 with open('inventory.csv', 'r') as csv_file:
     inventory = csv.reader(csv_file)
     next(inventory)
@@ -77,22 +92,62 @@ with open('inventory.csv', 'r') as csv_file:
     all_inventory = []
     
     #enters for loop and cleans the csv data 
-    for line in inventory:
-        product_quantity.append(int(line[2]))
-        all_inventory.append(line[0])
-        product_price.append(int(re.sub('[^0-9]','',line[1])))
-        date_updated.append(line[3])
+    for items in inventory:
+        all_stock = int(items[2])
+        all_products = items[0]
+        dates = items[3]
+        price = int(re.sub('[^0-9]','',items[1]))
+
+        all_inventory.append(all_products)
+        product_quantity.append(all_stock)
+        date_updated.append(dates)
+        product_price.append(price)
     
-    #has all information ready to use    
+    #has all information ready to use  
+
     inventory = [
         {'quantity': product_quantity},
         {'price': product_price},
         {'updated': date_updated},
         {'all_products': all_inventory}
     ]
+    
+           
+#def clean_data():
+#    for dict in inventory:
+#        for value in dict.values():
+#            print('\n')
+#            for item in value:
+#                print(item)
+#clean_data()
+
+
+
+
+
+
+
+
+
+#for price in product_price:
+#    print(price)
+#print('\n')
+#for item in all_inventory:
+#    print(item)  
+#print('\n')
+#for number in product_quantity:
+#    print(number)
+#print('\n')
+#for date in date_updated:
+#    print(date)
+    
+    
+    
+
 
 if __name__ == '__main__':
     #connects and creates the database and table if it was not created
     db.connect()
     db.create_tables([Product], safe = True)
-    all_product_names()
+    #view_all_inventory()
+    all_product_stock()
