8d4e3a6348386e7395473739af1f51e00f0093a5
David Garcia <44665014+OXDavidXO@users.noreply.github.com>
2020-12-14 01:38:05 -0600

    Update app.py
    simplified db need to add non duplicating solution

--- app.py
+++ app.py
@@ -6,48 +6,29 @@ import re
 
 db = SqliteDatabase('inventory.db')
 class Product(Model):
-    product_quantity = IntegerField(unique = True)
-    product_prices = IntegerField(unique = True)
-    product_names = CharField(unique = True)
+    product_quantity = IntegerField()
+    product_prices = IntegerField()
+    product_names = CharField()
     class Meta():
         database = db
-food_names = []
-food_price = []
-food_stock = []
-dates_added = []
-
-with open('inventory.csv', 'r') as csv_file:
-    file_reader = csv.reader(csv_file)
-    next(file_reader)
-    for item in file_reader:
-        foods = item[0]
-        food_cost = int(re.sub('[^0-9]','',item[1]))
-        item_stock = int(item[2])
-        added_dates = item[3]
-
-        food_names.append(foods)    
-        food_price.append(food_cost)
-        food_stock.append(item_stock)
-        dates_added.append(added_dates)
         
-
-inventory = {
-    'items': food_names,
-    'price': food_price,
-    'stock': food_stock,
-    'dates': dates_added
-}
-
-
-def add_products():
-    try:
-        food_item = Product.create(product_names = inventory['items'])
-        food_item.save()
-    except IntegrityError:
-        food_product = Product.get(product_names = inventory['items'])
-        
+def add_inventory():
+    with open('inventory.csv', 'r') as csv_file:
+        file_reader = csv.reader(csv_file)
+        next(file_reader)
+        for item in file_reader:
+            foods = item[0]
+            food_cost = int(re.sub('[^0-9]','',item[1]))
+            item_stock = int(item[2])
+            added_dates = item[3]
+            try:
+                add_data = Product.create(product_quantity = item_stock, product_prices = food_cost, product_names = foods)
+                add_data.save()
+            except IntegrityError:
+                food_data = Product.get(product_quantity = item_stock, product_prices = food_cost, product_names = foods)
+                
 
 if __name__ == '__main__':
     db.connect()
     db.create_tables([Product], safe = True)
-    add_products()
+    add_inventory()
